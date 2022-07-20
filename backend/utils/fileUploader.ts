3c022e69fa4ac4a7f2119c69b5e2422b8ac0ead8
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-20 18:10:05 +0200

    added cloudinary result type

--- backend/utils/fileUploader.ts
+++ backend/utils/fileUploader.ts
@@ -1,6 +1,9 @@
 const cloudinary = require("cloudinary").v2;
+import { CloudinaryUploadResult } from "../types/fileTypes";
 import checkFormat from "./checkFormat";
-const fileUploader = async (file: Express.Multer.File) => {
+const fileUploader = async (
+  file: Express.Multer.File
+): Promise<CloudinaryUploadResult> => {
   try {
     const fileUploaded = await cloudinary.uploader.upload(file.path, {
       folder: "library-files",
@@ -8,8 +11,9 @@ const fileUploader = async (file: Express.Multer.File) => {
       format: checkFormat(file.mimetype),
     });
     return fileUploaded;
-  } catch (err) {
-    console.log("Cloudinary error: ", err);
+  } catch (err: any) {
+    throw new Error(err.message);
+    return err;
   }
 };
 
