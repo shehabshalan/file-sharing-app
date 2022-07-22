b3a51b56c48fe7794d16a1d3547c0505d6242741
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-22 08:48:11 +0200

    home page edited

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,14 @@
 import { CssBaseline, Grid } from "@mui/material";
 import React from "react";
 import LeftImage from "../components/LeftImage";
-import UploadForm from "../components/UploadForm";
+import UploadSection from "../components/UploadSection";
 
 function Home() {
   return (
     <Grid container component="main" sx={{ height: "100vh" }}>
       <CssBaseline />
       <LeftImage />
-      <UploadForm />
+      <UploadSection />
     </Grid>
   );
 }
