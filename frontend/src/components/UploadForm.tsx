8c904ebf7eecabf1b51142e5c399b0da194e284f
Shehab Shalan <shehab.shalan@hotmail.com>
2022-07-22 08:47:22 +0200

    upload form updated

--- frontend/src/components/UploadForm.tsx
+++ frontend/src/components/UploadForm.tsx
@@ -1,98 +1,106 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { styled } from "@mui/material/styles";
 import AddIcon from "@mui/icons-material/Add";
-
-import BackupIcon from "@mui/icons-material/Backup";
+import DateAndTimePicker from "./DateAndTimePicker";
+import FileList from "./FileList";
+import { useUserContext } from "../context/UserContext";
+import { Alert, AlertTitle, LoadingButton } from "@mui/lab";
 
 const Input = styled("input")({
   display: "none",
 });
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      File Sharing App {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+
 function UploadForm() {
-  const [file, setFile] = React.useState<File | undefined>(undefined);
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFile(event.target.files?.[0]);
-  };
+  const {
+    handleFileChange,
+    filesUploaded,
+    handleSubmit,
+    uploading,
+    expirationDateTime,
+    sharingLink,
+    sharingError,
+    sharingSuccess,
+  } = useUserContext();
 
   return (
-    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-      <Box
-        sx={{
-          my: 8,
-          mx: 4,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          minHeight: "80vh",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Upload files here
+    <Box
+      sx={{
+        mb: 5,
+        margin: "auto",
+        marginTop: "2rem",
+        marginBottom: "2rem",
+        padding: "2rem",
+      }}
+    >
+      <Typography component="h1" variant="h6" align="center">
+        Upload files to share with others in 3 easy steps
+      </Typography>
+
+      <FileList />
+      <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 4 }}>
+        <Typography component="p" variant="body1" align="left" mb={4}>
+          <br />
+          <strong>1.</strong> Select files to upload
         </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 4 }}>
-          <label htmlFor="contained-button-thumbnail">
-            <Input
-              accept=".doc, .pdf, .docx, .txt, .csv, .xls, .xlsx, image/*"
-              id="contained-button-thumbnail"
-              name="myfile"
-              onChange={handleChange}
-              type="file"
-              multiple
-            />
-            <Button
-              sx={{ width: "100%" }}
-              variant="outlined"
-              component="span"
-              fullWidth
-            >
-              <AddIcon />
-            </Button>
-          </label>
-          {file && (
-            <Button
+        <label htmlFor="contained-button-thumbnail">
+          <Input
+            accept=".doc, .pdf, .docx, .txt, .csv, .xls, .xlsx, image/*"
+            id="contained-button-thumbnail"
+            name="myfile"
+            onChange={handleFileChange}
+            type="file"
+            multiple
+          />
+          <Button variant="outlined" component="span" fullWidth>
+            <AddIcon />
+          </Button>
+        </label>
+
+        {filesUploaded.length > 0 && (
+          <>
+            <Typography component="p" variant="body1" align="left" mb={4}>
+              <br />
+              <strong>2.</strong> Set expiration date and time
+            </Typography>
+            <DateAndTimePicker />
+          </>
+        )}
+
+        {expirationDateTime && (
+          <>
+            <Typography component="p" variant="body1" align="left">
+              <br />
+              <strong>3.</strong> Click "Upload"
+            </Typography>
+            <LoadingButton
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+              loading={uploading}
+              sx={{ mt: 4, mb: 2 }}
             >
               Upload
-            </Button>
-          )}
-
-          <Copyright sx={{ mt: 5 }} />
-        </Box>
+            </LoadingButton>
+          </>
+        )}
       </Box>
-    </Grid>
+      {sharingError && (
+        <Alert severity="error" sx={{ mt: 4 }}>
+          <AlertTitle>Error</AlertTitle>
+          An error occurred while uploading your files.
+        </Alert>
+      )}
+      {sharingSuccess && (
+        <Alert severity="success" sx={{ mt: 4 }}>
+          <AlertTitle> Here is your link</AlertTitle>
+          <Typography>
+            <a href={sharingLink}>{sharingLink}</a>
+          </Typography>
+        </Alert>
+      )}
+    </Box>
   );
 }
 
